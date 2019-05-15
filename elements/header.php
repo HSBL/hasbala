738f5e951beb0e0fa52aaf4f5d7589cc28a6b8f5
HSBL <46879920+HSBL@users.noreply.github.com>
2019-05-16 06:24:05 +0700

    Create header.php

---
+++ elements/header.php
@@ -0,0 +1,35 @@
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Portofolio - Amil Hasbala</title>
+  <link rel="shortcut icon" type="image/png" href="https://image.flaticon.com/icons/svg/511/511128.svg"/>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+  <style>
+
+  </style>
+</head>
+
+<body>
+<nav>
+    <div class="nav-wrapper teal">
+      <a href="#" class="brand-logo">Amil [Has]bala</a>
+      <a href="#" data-target="mobile-nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html">Service</a></li>
+        <li><a href="badges.html">Portfolio</a></li>
+        <li><a href="collapsible.html">About</a></li>
+        <li><a href="mobile.html">Contact</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <ul class="sidenav" id="mobile-nav">
+    <li><a href="sass.html">Service</a></li>
+    <li><a href="badges.html">Portfolio</a></li>
+    <li><a href="collapsible.html">About</a></li>
+    <li><a href="mobile.html">Contact</a></li>
+  </ul>
